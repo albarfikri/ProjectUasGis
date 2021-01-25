e62c8da01a4fcbb184f814265a237dc01cd62e3a
albarfikri4 <fikrialbar4@gmail.com>
2021-01-25 12:10:16 +0700

    Project selesai

--- application/views/pemetaan/pemetaan.php
+++ application/views/pemetaan/pemetaan.php
@@ -26,7 +26,7 @@
           <div class="row">
               <div class="col">
 
-                  <div id="mapid" style="width: stretch; height: 590px;"></div>
+                  <div id="mapid" style="width: stretch; height: 700px;"></div>
                   <script>
                       // Jenis Peta
                       var peta1 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
